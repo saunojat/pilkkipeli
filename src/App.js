dd46391b39ff549d287009a445434009c87eab3f
Lauri Matikainen <lauri_matikainen@hotmail.com>
2022-02-23 09:42:41 +0200

    lisää src

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React, {useState} from 'react'
-
-const OnJaEi = (props) => {
-  return (<div><p className="note">Onko Pekka isompi kuin Pätkä?
-  <button onClick={() => props.setValue(1)}>On</button>
-  <button onClick={() => props.setValue(0)}>Ei</button></p></div>)
-}
-
-const Alkuun = (props) => {
-  return (<div><p className="note"><button onClick={'/'}>Alkuun</button></p></div>)
-}
+import React from 'react'
 
 const App = () => {
 
-  const [value, setValue] = useState(0)
-  
+var randNum;
+var limit = Math.floor(Math.random()*(33-25)+25);;
+console.log("limit: ", limit)
+var bodytemp = 35;
+var fishes = 0;
+var fishbet = "y";
 
-if (value === 1) {
-  return(<div>
-    <OnJaEi setValue={setValue}/>
-    Pekka on isompi kuin Pätkä. 
-    <Alkuun />
-    </div>
-  )
-} else if (value === 0) {
-  return(<div>
-    <OnJaEi setValue={setValue} />
-    Pätkä on isompi kuin Pekka.
-    <Alkuun />
-  </div>)
+while (bodytemp > limit && fishbet === 'y') {
+randNum = Math.floor(Math.random()*2+1);
+console.log("rand-luku: ", randNum)
+limit = Math.floor(Math.random()*(33-25)+25);
+console.log("new limit: ", limit)
+fishbet = prompt("Do you want to ice fish for a while? Press y (yes) or no (n).");
+if (randNum === 1)
+{
+bodytemp--;
+fishes++;
+alert("Got a fish! Your body temperature: " + bodytemp);
 }
+else if (randNum === 2)
+{
+bodytemp--;
+alert("No bites atm. Your body temperature: " + bodytemp);
+}
+}
+
+if (fishbet === 'n' && fishes === 0)
+{
+alert("Leaving so soon?");
+}
+else if (fishbet === 'n' && fishes > 0)
+{
+alert("A fine decision to return to the shore when you still can. You got " + fishes + "  fishes with you!");
+}
+else if (fishbet === 'y' && bodytemp === limit) 
+{
+alert("Brrr! You reached hypothermy and also lost all your " + fishes + " fishes!");
+}
+
+
+return (
+<div>
+<img src="/pilkkichat_tausta.jpg" alt="pilkkichat"></img>
+</div>
+);
 }
 
 export default App;
