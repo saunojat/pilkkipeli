a84aba2cad335b59058e44323f53fe38d6aa768c
Lauri Matikainen <lauri_matikainen@hotmail.com>
2022-02-23 12:21:28 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
-import React from 'react'
+import React, {useState} from 'react'
+import logo from './eikalaa.gif'
+import logo2 from './kalaa.gif'
+import background1 from './tausta1.png'
+import background2 from './tausta2.png'
+
+const Question = (props) => {
+return (<div><p className="note">Haluatko pilkkiä tovin?
+<button onClick={() => props.setValue(1)}>Kyllä</button>
+<button onClick={() => props.setValue(0)}>En</button></p></div>)
+}
+
+const Background1 = (props) => {
+return(<div>
+<img width="400px" src={background1} alt="loading..." />
+</div>)
+}
+
+const Background2 = (props) => {
+return(<div><img width="400px" src={background2} alt="loading..." /></div>)
+}
 
 const App = () => {
 
-var randNum;
-var limit = Math.floor(Math.random()*(33-25)+25);;
-console.log("limit: ", limit)
-var bodytemp = 35;
-var fishes = 0;
-var fishbet = "y";
+const [value, setValue] = useState(1)
+let bodytemp = 36;
 
-while (bodytemp > limit && fishbet === 'y') {
-randNum = Math.floor(Math.random()*2+1);
-console.log("rand-luku: ", randNum)
-limit = Math.floor(Math.random()*(33-25)+25);
-console.log("new limit: ", limit)
-fishbet = prompt("Do you want to ice fish for a while? Press y (yes) or no (n).");
-if (randNum === 1)
+while (bodytemp > 25)
 {
+var randNum = Math.floor(Math.random()*5+1);
+console.log("random-luku: ", randNum)
+
+if (value === 1 && (randNum === 1 || randNum === 2 || randNum === 3)) {
+console.log("ei tärppejä")
 bodytemp--;
-fishes++;
-alert("Got a fish! Your body temperature: " + bodytemp);
-}
-else if (randNum === 2)
-{
-bodytemp--;
-alert("No bites atm. Your body temperature: " + bodytemp);
-}
-}
-
-if (fishbet === 'n' && fishes === 0)
-{
-alert("Leaving so soon?");
-}
-else if (fishbet === 'n' && fishes > 0)
-{
-alert("A fine decision to return to the shore when you still can. You got " + fishes + "  fishes with you!");
-}
-else if (fishbet === 'y' && bodytemp === limit) 
-{
-alert("Brrr! You reached hypothermy and also lost all your " + fishes + " fishes!");
-}
-
-
-return (
-<div>
-<img src="/pilkkichat_tausta.jpg" alt="pilkkichat"></img>
+randNum = Math.floor(Math.random()*5+1);
+console.log("uusi random-luku, ei tärppejä: ", randNum)
+return(<div>
+<Question setValue={setValue}/>
+{console.log("value: ", value, "setValue: ", setValue)}
+Ei tärppejä. Ruumiinlämpösi on {bodytemp} C. 
+<Background1 />
+<img width="400px" src={logo} alt="loading..." />
+<Background2 />
 </div>
-);
+)
+} else if (value === 1 && (randNum === 4 || randNum === 5)) {
+console.log("sait kalan")
+bodytemp--;
+randNum = Math.floor(Math.random()*5+1);
+console.log("uusi random-luku, sai kalan: ", randNum)
+return(<div>
+<Question setValue={setValue} />
+Sait kalan! Ruumiinlämpösi on {bodytemp} C.
+<Background1 />
+<img width="400px" src={logo2} alt="loading..." />
+<Background2 />
+</div>)
+} else if (value === 0) {
+return(<div>Mikä hätänä?</div>)
+}
+}
 }
 
 export default App;
